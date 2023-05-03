256d348cbb6756f930d02d7e09f5da470ed49ad0
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-05-03 14:59:39 +0300

    removed some filters

--- ckanext/saeoss/logic/action/saeoss.py
+++ ckanext/saeoss/logic/action/saeoss.py
@@ -7,17 +7,14 @@ import ckan.plugins.toolkit as toolkit
 import sqlalchemy
 
 from ... import jobs
-from ...constants import DatasetManagementActivityType
-
-from . import create_dataset_management_activity
 
 logger = logging.getLogger(__name__)
 
 
 @toolkit.side_effect_free
 def show_version(
-    context: typing.Optional[typing.Dict] = None,
-    data_dict: typing.Optional[typing.Dict] = None,
+        context: typing.Optional[typing.Dict] = None,
+        data_dict: typing.Optional[typing.Dict] = None,
 ) -> typing.Dict:
     """return the current version of this project"""
     return {
@@ -28,8 +25,8 @@ def show_version(
 
 @toolkit.side_effect_free
 def list_featured_datasets(
-    context: typing.Dict,
-    data_dict: typing.Optional[typing.Dict] = None,
+        context: typing.Dict,
+        data_dict: typing.Optional[typing.Dict] = None,
 ) -> typing.List:
     toolkit.check_access("authorize_list_featured_datasets", context, data_dict)
     data_ = data_dict.copy() if data_dict is not None else {}
@@ -51,41 +48,6 @@ def list_featured_datasets(
         .offset(offset)
     )
     return [r for r in query.execute()]
-
-
-def request_dataset_maintenance(context: typing.Dict, data_dict: typing.Dict):
-    """Request that a dataset be put on maintenance mode (AKA make it private)
-
-    This action performs the following:
-
-    - Create a new activity, so that it shows up on the user dashboard
-    - Enqueue background job which will email the dataset's owner_org admins
-    - Ensure user is registered to receive email notifications
-    - Ensure user is following the dataset
-
-    """
-
-    toolkit.check_access("request_dataset_maintenance", context, data_dict)
-    activity = create_dataset_management_activity(
-        data_dict["pkg_id"], DatasetManagementActivityType.REQUEST_MAINTENANCE
-    )
-    _ensure_user_is_notifiable(context["user"], data_dict["pkg_id"])
-    toolkit.enqueue_job(
-        jobs.notify_org_admins_of_dataset_management_request,
-        args=[activity["id"]],
-    )
-
-
-def request_dataset_publication(context: typing.Dict, data_dict: typing.Dict):
-    toolkit.check_access("request_dataset_publication", context, data_dict)
-    activity = create_dataset_management_activity(
-        data_dict["pkg_id"], DatasetManagementActivityType.REQUEST_PUBLICATION
-    )
-    _ensure_user_is_notifiable(context["user"], data_dict["pkg_id"])
-    toolkit.enqueue_job(
-        jobs.notify_org_admins_of_dataset_management_request,
-        args=[activity["id"]],
-    )
 
 
 def _ensure_user_is_notifiable(user_id: str, dataset_id):
