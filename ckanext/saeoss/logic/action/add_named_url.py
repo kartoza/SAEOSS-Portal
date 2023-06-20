a27960b20cf0dc1772fac3243d409009c1584163
mohab <homab3@gmail.com>
2023-06-20 12:44:00 +0300

    adding new naming rules for dataset

--- ckanext/saeoss/logic/action/add_named_url.py
+++ ckanext/saeoss/logic/action/add_named_url.py
@@ -1,19 +1,34 @@
 import uuid
+import re
+import ckan.plugins.toolkit as tk
 
 
-def handle_named_url(data_dict: dict):
+def populate_dataset_name(data_dict: dict, context: dict, calls:int = 0):
     """
     adding uuid to the named url
     , the name field was a bit
     confusing to the client.
     """
     title = data_dict.get("title")
-    name = _remove_special_characters_from_package_url(title)
-    if name is not None:
-        name = name.replace(" ", "-")
-        name += "-" + str(uuid.uuid4())
-        name = name.lower()
+    if data_dict.get('name'):
+        name = _remove_special_characters_from_package_url(data_dict.get('name'))
+    else:
+        name = _remove_special_characters_from_package_url(title)
+    
+    validation_dict = {"name":name}
+    schema = {"name": [tk.get_validator("package_name_exists")]}
+    data,error = tk.navl_validate(validation_dict,schema, context)
+    if error:
         return name
+
+    calls += 1
+    if re.search(r'-\d$',name):
+        name = re.sub(r'-\d$', '', name)
+        name = name+'-'+str(calls)
+    else:
+        name = name+'-'+str(calls)
+    data_dict['name'] = name
+    return populate_dataset_name(data_dict, context, calls)
 
 
 def _remove_special_characters_from_package_url(url):
@@ -22,7 +37,7 @@ def _remove_special_characters_from_package_url(url):
     accepted by CKAN for dataset
     urls, replace them
     """
-    special_chars = "!\"”'#$%&'()*+,-./:;<=>?@[\]^`{|}~.[]"
+    special_chars = "!\"”'#$%&'()*+,-./:;<=>?@[\]^`{|}~.[] "
     if url is not None:
         for i in url:
             if i in special_chars:
