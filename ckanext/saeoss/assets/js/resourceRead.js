98fb38a572d76c8a7ef68b3bc2ac21f28d25b1e0
voogt <47378310+voogt@users.noreply.github.com>
2023-09-07 12:04:14 +0200

    update resource read

--- ckanext/saeoss/assets/js/resourceRead.js
+++ ckanext/saeoss/assets/js/resourceRead.js
@@ -107,21 +107,21 @@ else if(ext_type == "kml"){
     .catch(err => console.log(err))
     omnivore.kml(file).addTo(map);
 }
-else if(ext_type == 'csv'){
-    let table = document.getElementById("demoTable");
+// else if(ext_type == 'csv'){
+//     let table = document.getElementById("demoTable");
 
-    fetch(file)
-    .then(res => res.text())
-    .then(csv => {
-    table.innerHTML = "";
-    for (let row of CSV.parse(csv)) {
-        let tr = table.insertRow();
-        for (let col of row) {
-            let td = tr.insertCell();
-            td.innerHTML = col;
-        }
-    }
-    });
-    omnivore.csv(file).addTo(map);
-}
+//     fetch(file)
+//     .then(res => res.text())
+//     .then(csv => {
+//     table.innerHTML = "";
+//     for (let row of CSV.parse(csv)) {
+//         let tr = table.insertRow();
+//         for (let col of row) {
+//             let td = tr.insertCell();
+//             td.innerHTML = col;
+//         }
+//     }
+//     });
+//     omnivore.csv(file).addTo(map);
+// }
 
