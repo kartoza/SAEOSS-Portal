b51ee950fcc6f07c826b1f9b24acd05c77a6d127
Jeff Osundwa <jeff@kartoza.com>
2024-01-25 00:25:47 +0300

    test: create admin and test playwright authentication

---
+++ playwright/ci-test/tests/00-auth.setup.ts
@@ -0,0 +1,61 @@
+import { test as setup, expect } from '@playwright/test';
+
+let url = '/';
+
+let username = 'admin';
+let useremail = 'admin@example.com';
+let password = '12345678';
+const authFile = 'auth.json'
+
+
+setup('test', async ({ page }) => {
+  await page.goto(url);
+
+  await expect(page.locator('.navbar-brand')).toBeVisible();
+
+  await expect(page.getByText('Welcome to the SAEOSS Portal LOGIN SIGN UP METADATA Discover a world of data-')).toBeVisible();
+
+  await expect(page.getByText('Welcome to the SAEOSS Portal')).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'LOGIN' })).toBeVisible();
+
+  await expect(page.locator('#title-header').getByRole('link', { name: 'SIGN UP' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'LOGIN' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+  await expect(page.getByRole('heading', { name: 'Need an Account?' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Create an Account' })).toBeVisible();
+
+  await expect(page.getByRole('heading', { name: 'Forgotten your password?' })).toBeVisible();
+
+  await expect(page.locator('p').filter({ hasText: 'Forgot your password?' })).toBeVisible();
+
+  await page.getByLabel('Username').click();
+
+  await page.getByLabel('Username').fill('admin');
+
+  await page.getByLabel('Password').click();
+
+  await page.getByLabel('Password').fill('admin123');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('heading', { name: 'News feed Activity from items' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' News feed' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' My Metadata Records' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' My Organisations' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' My Groups' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' Profile settings' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Activity from: Everything' })).toBeVisible();
+
+  await page.context().storageState({ path: authFile });
+});
