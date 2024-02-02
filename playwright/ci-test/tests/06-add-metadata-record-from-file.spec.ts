cb0f4716c6ecbd084efff08b5d7fc10cd79450ee
Jeff Osundwa <jeff@kartoza.com>
2024-02-02 17:32:37 +0300

    test: add tests for yaml and json files

--- playwright/ci-test/tests/06-add-metadata-record-from-file.spec.ts
+++ playwright/ci-test/tests/06-add-metadata-record-from-file.spec.ts
@@ -6,66 +6,211 @@ test.use({
   storageState: 'auth.json'
 });
 
-test('test', async ({ page }) => {
-  await page.goto(url);
+test.describe('add metadata record from file', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto(url);
+  });
 
-  await expect(page.getByText('Welcome to the SAEOSS Portal METADATA Discover a world of data-driven')).toBeVisible();
+  test('XML', async ({ page }) => {
 
-  await expect(page.locator('#title-header')).toContainText('Welcome to the SAEOSS Portal');
+    await expect(page.getByText('Welcome to the SAEOSS Portal METADATA Discover a world of data-driven')).toBeVisible();
 
-  await expect(page.getByRole('link', { name: 'METADATA', exact: true })).toBeVisible();
+    await expect(page.locator('#title-header')).toContainText('Welcome to the SAEOSS Portal');
 
-  await expect(page.getByRole('heading')).toContainText('Discover a world of data-driven possibilities at the SAEOSS-Portal, where information converges to empower data sharing and decision making.');
+    await expect(page.getByRole('link', { name: 'METADATA', exact: true })).toBeVisible();
 
-  await expect(page.locator('.navbar-brand')).toBeVisible();
+    await expect(page.getByRole('heading')).toContainText('Discover a world of data-driven possibilities at the SAEOSS-Portal, where information converges to empower data sharing and decision making.');
 
-  await expect(page.getByTestId('loggedin_user_icon')).toBeVisible();
+    await expect(page.locator('.navbar-brand')).toBeVisible();
 
-  await page.getByRole('link', { name: 'METADATA', exact: true }).click();
+    await expect(page.getByTestId('loggedin_user_icon')).toBeVisible();
 
-  await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+    await page.getByRole('link', { name: 'METADATA', exact: true }).click();
 
-  await expect(page.getByRole('link', { name: 'Add metadata record' })).toBeVisible();
+    await page.waitForURL('**/dataset/');
 
-  await expect(page.getByRole('button', { name: ' Add metadata record from' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
 
-  const fileChooserPromise = page.waitForEvent('filechooser');
+    await expect(page.getByRole('link', { name: 'Add metadata record' })).toBeVisible();
 
-  await page.getByRole('button', { name: ' Add metadata record from' }).click();
+    await expect(page.getByRole('button', { name: ' Add metadata record from' })).toBeVisible();
 
-  const fileChooser = await fileChooserPromise;
+    const fileChooserPromise = page.waitForEvent('filechooser');
 
-  await fileChooser.setFiles('tests/fixtures/SANS1878.xml');
+    await page.getByRole('button', { name: ' Add metadata record from' }).click();
 
-  await expect(page.getByText('all packages were created×')).toBeVisible();
+    const fileChooser = await fileChooserPromise;
 
-  await page.getByRole('link', { name: '×' }).click();
+    await fileChooser.setFiles('tests/fixtures/SANS1878.xml');
 
-  await page.reload();
+    await page.waitForLoadState('load');
 
-  await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+    await expect(page.getByText('all packages were created×')).toBeVisible();
 
-  await expect(page.getByLabel('Breadcrumb').getByRole('list')).toContainText('3 metadata records found');
+    await page.getByRole('link', { name: '×' }).click();
 
-  await expect(page.locator('li').filter({ hasText: 'test 2 Abstract from the' })).toBeVisible();
+    await page.reload();
 
-  await page.locator('#content div').filter({ hasText: 'test 2 Abstract from the' }).nth(2).click();
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
 
-  await page.getByRole('link', { name: 'test 2' }).click();
+    await expect(page.getByLabel('Breadcrumb').getByRole('list')).toContainText('3 metadata records found');
 
-  await expect(page.getByRole('heading', { name: 'test 2' })).toBeVisible();
+    await expect(page.locator('li').filter({ hasText: 'test xml Abstract from the' })).toBeVisible();
 
-  await expect(page.getByRole('heading', { name: 'Dataset extent' })).toBeVisible();
+    await page.locator('#content div').filter({ hasText: 'test xml Abstract from the' }).nth(2).click();
 
-  await expect(page.getByRole('link', { name: ' Manage' })).toBeVisible();
+    await page.getByRole('link', { name: 'test xml' }).click();
 
-  await page.getByRole('link', { name: ' Groups' }).click();
+    await expect(page.getByRole('heading', { name: 'test xml' })).toBeVisible();
 
-  await page.getByRole('link', { name: ' Activity Stream' }).click();
+    await expect(page.getByRole('heading', { name: 'Dataset extent' })).toBeVisible();
 
-  await expect(page.getByText('admin created the metadata')).toBeVisible();
+    await expect(page.getByRole('link', { name: ' Manage' })).toBeVisible();
 
-  await page.getByRole('link', { name: 'Metadata', exact: true }).click();
+    await page.getByRole('link', { name: ' Groups' }).click();
 
-  await expect(page.getByRole('heading', { name: 'test 2' })).toBeVisible();
+    await page.getByRole('link', { name: ' Activity Stream' }).click();
+
+    await expect(page.getByText('admin created the metadata')).toBeVisible();
+
+    await page.getByRole('link', { name: 'Metadata', exact: true }).first().click();
+
+    await expect(page.getByRole('heading', { name: 'test xml' })).toBeVisible();
+  });
+
+  test('JSON', async ({ page }) => {
+
+    await expect(page.getByText('Welcome to the SAEOSS Portal METADATA Discover a world of data-driven')).toBeVisible();
+
+    await expect(page.locator('#title-header')).toContainText('Welcome to the SAEOSS Portal');
+
+    await expect(page.getByRole('link', { name: 'METADATA', exact: true })).toBeVisible();
+
+    await expect(page.getByRole('heading')).toContainText('Discover a world of data-driven possibilities at the SAEOSS-Portal, where information converges to empower data sharing and decision making.');
+
+    await expect(page.locator('.navbar-brand')).toBeVisible();
+
+    await expect(page.getByTestId('loggedin_user_icon')).toBeVisible();
+
+    await page.getByRole('link', { name: 'METADATA', exact: true }).click();
+
+    await page.waitForURL('**/dataset/');
+
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: 'Add metadata record' })).toBeVisible();
+
+    await expect(page.getByRole('button', { name: ' Add metadata record from' })).toBeVisible();
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+
+    await page.getByRole('button', { name: ' Add metadata record from' }).click();
+
+    const fileChooser = await fileChooserPromise;
+
+    await fileChooser.setFiles('tests/fixtures/SANS1878.json');
+
+    await page.waitForLoadState('load');
+
+    await expect(page.getByText('all packages were created×')).toBeVisible();
+
+    await page.getByRole('link', { name: '×' }).click();
+
+    await page.reload();
+
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+    await expect(page.getByLabel('Breadcrumb').getByRole('list')).toContainText('4 metadata records found');
+
+    await expect(page.locator('li').filter({ hasText: 'test json Abstract from the' })).toBeVisible();
+
+    await page.locator('#content div').filter({ hasText: 'test json Abstract from the' }).nth(2).click();
+
+    await page.getByRole('link', { name: 'test json' }).click();
+
+    await expect(page.getByRole('heading', { name: 'test json' })).toBeVisible();
+
+    await expect(page.getByRole('heading', { name: 'Dataset extent' })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: ' Manage' })).toBeVisible();
+
+    await page.getByRole('link', { name: ' Groups' }).click();
+
+    await page.getByRole('link', { name: ' Activity Stream' }).click();
+
+    await expect(page.getByText('admin created the metadata')).toBeVisible();
+
+    await page.getByRole('link', { name: 'Metadata', exact: true }).first().click();
+
+    await expect(page.getByRole('heading', { name: 'test json' })).toBeVisible();
+  });
+
+  test('YAML', async ({ page }) => {
+
+    await expect(page.getByText('Welcome to the SAEOSS Portal METADATA Discover a world of data-driven')).toBeVisible();
+
+    await expect(page.locator('#title-header')).toContainText('Welcome to the SAEOSS Portal');
+
+    await expect(page.getByRole('link', { name: 'METADATA', exact: true })).toBeVisible();
+
+    await expect(page.getByRole('heading')).toContainText('Discover a world of data-driven possibilities at the SAEOSS-Portal, where information converges to empower data sharing and decision making.');
+
+    await expect(page.locator('.navbar-brand')).toBeVisible();
+
+    await expect(page.getByTestId('loggedin_user_icon')).toBeVisible();
+
+    await page.getByRole('link', { name: 'METADATA', exact: true }).click();
+
+    await page.waitForURL('**/dataset/');
+
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: 'Add metadata record' })).toBeVisible();
+
+    await expect(page.getByRole('button', { name: ' Add metadata record from' })).toBeVisible();
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+
+    await page.getByRole('button', { name: ' Add metadata record from' }).click();
+
+    const fileChooser = await fileChooserPromise;
+
+    await fileChooser.setFiles('tests/fixtures/SANS1878.yaml');
+
+    await page.waitForLoadState('load');
+
+    await expect(page.getByText('all packages were created×')).toBeVisible();
+
+    await page.getByRole('link', { name: '×' }).click();
+
+    await page.reload();
+
+    await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+    await expect(page.getByLabel('Breadcrumb').getByRole('list')).toContainText('5 metadata records found');
+
+    await expect(page.locator('li').filter({ hasText: 'test yaml Abstract from the' })).toBeVisible();
+
+    await page.locator('#content div').filter({ hasText: 'test yaml Abstract from the' }).nth(2).click();
+
+    await page.getByRole('link', { name: 'test yaml' }).click();
+
+    await expect(page.getByRole('heading', { name: 'test yaml' })).toBeVisible();
+
+    await expect(page.getByRole('heading', { name: 'Dataset extent' })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: ' Manage' })).toBeVisible();
+
+    await page.getByRole('link', { name: ' Groups' }).click();
+
+    await page.getByRole('link', { name: ' Activity Stream' }).click();
+
+    await expect(page.getByText('admin created the metadata')).toBeVisible();
+
+    await page.getByRole('link', { name: 'Metadata', exact: true }).first().click();
+
+    await expect(page.getByRole('heading', { name: 'test yaml' })).toBeVisible();
+  });
+
 });
