cf870baa4f6dac20878f5a8f51c4edd03cf9843d
Jeff Osundwa <jeff@kartoza.com>
2024-02-02 16:24:03 +0300

    test: add test for adding metadata from xml file

---
+++ playwright/ci-test/tests/06-add-metadata-record-from-file.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto(url);
+
+  await expect(page.getByText('Welcome to the SAEOSS Portal METADATA Discover a world of data-driven')).toBeVisible();
+
+  await expect(page.locator('#title-header')).toContainText('Welcome to the SAEOSS Portal');
+
+  await expect(page.getByRole('link', { name: 'METADATA', exact: true })).toBeVisible();
+
+  await expect(page.getByRole('heading')).toContainText('Discover a world of data-driven possibilities at the SAEOSS-Portal, where information converges to empower data sharing and decision making.');
+
+  await expect(page.locator('.navbar-brand')).toBeVisible();
+
+  await expect(page.getByTestId('loggedin_user_icon')).toBeVisible();
+
+  await page.getByRole('link', { name: 'METADATA', exact: true }).click();
+
+  await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Add metadata record' })).toBeVisible();
+
+  await expect(page.getByRole('button', { name: ' Add metadata record from' })).toBeVisible();
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+
+  await page.getByRole('button', { name: ' Add metadata record from' }).click();
+
+  const fileChooser = await fileChooserPromise;
+
+  await fileChooser.setFiles('tests/fixtures/SANS1878.xml');
+
+  await expect(page.getByText('all packages were created×')).toBeVisible();
+
+  await page.getByRole('link', { name: '×' }).click();
+
+  await page.reload();
+
+  await expect(page.getByRole('heading', { name: 'Metadata', exact: true })).toBeVisible();
+
+  await expect(page.getByLabel('Breadcrumb').getByRole('list')).toContainText('3 metadata records found');
+
+  await expect(page.locator('li').filter({ hasText: 'test 2 Abstract from the' })).toBeVisible();
+
+  await page.locator('#content div').filter({ hasText: 'test 2 Abstract from the' }).nth(2).click();
+
+  await page.getByRole('link', { name: 'test 2' }).click();
+
+  await expect(page.getByRole('heading', { name: 'test 2' })).toBeVisible();
+
+  await expect(page.getByRole('heading', { name: 'Dataset extent' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: ' Manage' })).toBeVisible();
+
+  await page.getByRole('link', { name: ' Groups' }).click();
+
+  await page.getByRole('link', { name: ' Activity Stream' }).click();
+
+  await expect(page.getByText('admin created the metadata')).toBeVisible();
+
+  await page.getByRole('link', { name: 'Metadata', exact: true }).click();
+
+  await expect(page.getByRole('heading', { name: 'test 2' })).toBeVisible();
+});
