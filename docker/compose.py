5687e9d2e2debe6487974c922608b6f73b7edf37
Jeff Osundwa <jeff@kartoza.com>
2025-02-10 16:06:04 +0300

    Update compose file to also use "docker compose"

--- docker/compose.py
+++ docker/compose.py
@@ -8,12 +8,24 @@ import os
 import shlex
 import sys
 import typing
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 logger = logging.getLogger(__name__)
 
 _FALLBACK_GIT_BRANCH = "main"
 _IMAGE_NAME = "saeoss"
+
+
+def _get_docker_compose_command() -> str:
+    """Check if `docker compose` is available, otherwise fallback to `docker-compose`."""
+    try:
+        check_output(shlex.split("docker compose version"), text=True)
+        return "docker compose"
+    except (FileNotFoundError, CalledProcessError):
+        return "docker-compose"
+
+
+DOCKER_COMPOSE_CMD = _get_docker_compose_command()
 
 
 def main():
@@ -39,10 +51,9 @@ def main():
 
 
 def run_compose_up(args):
-    # raise SystemExit(f"the args are {args}")
     image_tag = args.image_tag if args.image_tag else _get_image_tag_name()
     if image_tag is not None:
-        exec_env = _set_exec_environment(image_tag) # this what sets the CKAN_IMAGE_TAG env variable
+        exec_env = _set_exec_environment(image_tag)
         logger.info(f"Using {image_tag!r} as the tag for the CKAN image...")
         _run_docker_compose("up --detach", args.compose_file, exec_env)
     else:
@@ -55,65 +66,67 @@ def run_compose_up(args):
 
 def run_compose_down(args):
     image_tag = args.image_tag if args.image_tag else _get_image_tag_name()
-    if image_tag is not None:
-        exec_env = _set_exec_environment(image_tag)
-    else:
-        exec_env = os.environ.copy()
+    exec_env = (
+        _set_exec_environment(image_tag) if image_tag else os.environ.copy()
+    )
     _run_docker_compose("down", args.compose_file, exec_env)
 
 
 def run_compose_restart(args):
     _run_docker_compose(f"restart {' '.join(args.service)}", args.compose_file)
 
+
 def run_db_backup(args):
-    timestamp = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     env = os.environ.copy()
     command = _get_compose_command(
-        f"exec ckan-db su - postgres -c \"pg_dumpall\" | gzip -9 > ckan-{timestamp}.sql.gz",
-        args.compose_file)
-    logger.debug(
-        f"About to replace the current process with the one that results from running "
-        f"{command!r} with an environment of {env}"
+        f'exec ckan-db su - postgres -c "pg_dumpall" | gzip -9 > ckan-{timestamp}.sql.gz',
+        args.compose_file,
     )
+    logger.debug(f"Running: {command!r} with environment {env}")
     sys.stdout.flush()
     sys.stderr.flush()
     os.system(
-        f"docker-compose --project-name=saeoss exec ckan-db su - postgres -c \"pg_dumpall\" | "
-        f"gzip -9 > ckan-{timestamp}.sql.gz",
+        f'{DOCKER_COMPOSE_CMD} --project-name=saeoss exec ckan-db su - postgres -c "pg_dumpall" | '
+        f"gzip -9 > ckan-{timestamp}.sql.gz"
     )
     os.system(
-        f"docker-compose --project-name=saeoss exec ckan-db su - postgres -c \"pg_dumpall\" | "
-        f"gzip -9 > datastore-{timestamp}.sql.gz",
+        f'{DOCKER_COMPOSE_CMD} --project-name=saeoss exec ckan-db su - postgres -c "pg_dumpall" | '
+        f"gzip -9 > datastore-{timestamp}.sql.gz"
     )
 
 
 def _get_compose_command(fragment: str, compose_file: typing.List[str]) -> str:
     files_fragment = " ".join(f"--file={path}" for path in compose_file)
-    template = "docker-compose --project-name={project} {files} {fragment}"
-    return template.format(project="saeoss", files=files_fragment, fragment=fragment)
+    template = (
+        f"{DOCKER_COMPOSE_CMD} --project-name={{project}} {files_fragment} {fragment}"
+    )
+    return template.format(project="saeoss")
 
 
 def _get_image_tag_name() -> typing.Optional[str]:
-    current_git_branch = (
-        check_output(shlex.split("git rev-parse --abbrev-ref HEAD"), text=True) # the current branch name
-        .strip("\n")
-        .replace("/", "-")
-    )
-    existing_image_tags = check_output(
-        shlex.split(f"docker images {_IMAGE_NAME} --format '{{{{.Tag}}}}'"), text=True  # pretty much get the tag see https://docs.docker.com/engine/reference/commandline/images/#format
-    ).split("\n")
-    if current_git_branch in existing_image_tags:
-        logger.info("The current branch already has a built tag, lets use that")
-        result = current_git_branch
-    elif _FALLBACK_GIT_BRANCH in existing_image_tags:
-        logger.info(
-            f"The current branch does not have a built tag yet, lets use the "
-            f"{_FALLBACK_GIT_BRANCH!r} image tag"
+    try:
+        current_git_branch = (
+            check_output(shlex.split("git rev-parse --abbrev-ref HEAD"), text=True)
+            .strip("\n")
+            .replace("/", "-")
         )
-        result = _FALLBACK_GIT_BRANCH
-    else:
-        result = None
-    return result
+        existing_image_tags = check_output(
+            shlex.split(f"docker images {_IMAGE_NAME} --format '{{{{.Tag}}}}'"),
+            text=True,
+        ).split("\n")
+        if current_git_branch in existing_image_tags:
+            logger.info("The current branch already has a built tag, using that...")
+            return current_git_branch
+        elif _FALLBACK_GIT_BRANCH in existing_image_tags:
+            logger.info(
+                f"The current branch does not have a built tag,"
+                f" using {_FALLBACK_GIT_BRANCH!r} image tag."
+            )
+            return _FALLBACK_GIT_BRANCH
+    except CalledProcessError:
+        logger.error("Failed to retrieve git branch or docker images.")
+    return None
 
 
 def _set_exec_environment(image_tag: str) -> typing.Dict[str, str]:
@@ -129,15 +142,11 @@ def _run_docker_compose(
     environment: typing.Optional[typing.Dict[str, str]] = None,
 ):
     env = environment or os.environ.copy()
-    # raise SystemExit("the command fragment:", compose_file)
     command = _get_compose_command(command_fragment, compose_file)
-    logger.debug(
-        f"About to replace the current process with the one that results from running "
-        f"{command!r} with an environment of {env}"
-    )
+    logger.debug(f"Executing command: {command!r} with environment {env}")
     sys.stdout.flush()
     sys.stderr.flush()
-    os.execvpe("docker-compose", shlex.split(command), env)
+    os.execvpe(DOCKER_COMPOSE_CMD.split()[0], shlex.split(command), env)
 
 
 if __name__ == "__main__":
