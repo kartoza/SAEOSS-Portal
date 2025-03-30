396af450262f83fed68e405681f15e1127ff816a
voogt <47378310+voogt@users.noreply.github.com>
2025-03-30 15:24:28 +0200

    fix redirect on password reset submit (#382)
    * fix redirect on password reset submit
    
    * remove commented out code

--- ckanext/saeoss/blueprints/reset_password.py
+++ ckanext/saeoss/blueprints/reset_password.py
@@ -1,27 +1,36 @@
 import logging
 
-from ckan.common import config
+from flask import Blueprint
+from flask.views import MethodView
 from ckan.common import asbool
-from six import text_type
+from six import text_type, ensure_str
+import dominate.tags as dom_tags
 
+import ckan.lib.authenticator as authenticator
 import ckan.lib.base as base
-import ckan.model as model
-import ckan.lib.helpers as h
-import ckan.authz as authz
-import ckan.logic as logic
-import ckan.logic.schema as schema
 import ckan.lib.captcha as captcha
+import ckan.lib.helpers as h
 import ckan.lib.mailer as mailer
 import ckan.lib.navl.dictization_functions as dictization_functions
-import ckan.lib.authenticator as authenticator
-import ckan.plugins as p
+import ckan.logic as logic
+import ckan.logic.schema as schema
+import ckan.model as model
+import ckan.plugins as plugins
+from ckan import authz
+from ckan.common import _, config, g, request
+
+
+log = logging.getLogger(__name__)
+
+from ckan.views import user
 
 from ckan.common import _, c
 
 from flask import Blueprint, request
 
 log = logging.getLogger(__name__)
-
+new_user_form = u'user/new_user_form.html'
+edit_user_form = u'user/edit_user_form.html'
 
 abort = base.abort
 render = base.render
@@ -36,8 +45,8 @@ UsernamePasswordError = logic.UsernamePasswordError
 DataError = dictization_functions.DataError
 unflatten = dictization_functions.unflatten
 
-reset_blueprint = Blueprint(
-    "validator", __name__, template_folder="templates", url_prefix="/password_reset"
+reset_password_blueprint = Blueprint(
+    "password", __name__, url_prefix="/password"
 )
 
 def _lowercase(obj):
@@ -52,87 +61,87 @@ def _lowercase(obj):
     else:
         return obj
 
-@reset_blueprint.route("/", methods = ['POST', 'GET'])
-def request_reset():
-    context = {'model': model, 'session': model.Session, 'user': c.user,
-                'auth_user_obj': c.userobj}
-    data_dict = {'id': request.form.get('user')}
-    log.debug("sent from blueprint")
-    try:
-        check_access('request_reset', context)
-    except NotAuthorized:
-        abort(403, _('Unauthorized to request reset password.'))
+class RequestResetPasswordView(MethodView):
+    def _prepare(self):
+        context = {
+            u'model': model,
+            u'session': model.Session,
+            u'user': g.user,
+            u'auth_user_obj': g.userobj
+        }
+        try:
+            logic.check_access(u'request_reset', context)
+        except logic.NotAuthorized:
+            base.abort(403, _(u'Unauthorized to request reset password.'))
 
-    users = _lowercase(logic.get_action('user_list')(context, {}))
-    log.info(f"USERS {users}")
-
-    if request.method == 'POST':
-        id = request.form.get('user')
+    def post(self):
+        self._prepare()
+        id = request.form.get(u'user')
         if id in (None, u''):
             h.flash_error(_(u'Email is required'))
             return h.redirect_to(u'/user/reset')
-        context = {'model': model,
-                    'user': c.user,
-                    u'ignore_auth': True}
+
+        context = {u'model': model, u'user': g.user, u'ignore_auth': True}
         user_objs = []
 
-        if u'@' not in id:
-            try:
-                user_dict = get_action('user_show')(context, {'id': id})
-                user_objs.append(context['user_obj'])
-            except NotFound:
-                try:
-                    user_dict = get_action('user_show')(context, {'id': id.lower()})
-                    user_objs.append(context['user_obj'])
-                except NotFound:
-                    try:
-                        user_dict = get_action('user_show')(context, {'id': id.capitalize()})
-                        user_objs.append(context['user_obj'])
-                    except NotFound:
-                        log.info("USER NOT FOUND EXCEPT")
-                        pass
-        else:
+        # Usernames cannot contain '@' symbols
+        if u'@' in id:
+            # Search by email address
+            # (You can forget a user id, but you don't tend to forget your
+            # email)
             user_list = logic.get_action(u'user_list')(context, {
                 u'email': id
             })
-            if not user_list:
-                user_list = logic.get_action(u'user_list')(context, {
-                u'email': id.lower()})
-            if not user_list:
-                user_list = logic.get_action(u'user_list')(context, {
-                u'email': id.capitalize()})
             if user_list:
                 # send reset emails for *all* user accounts with this email
                 # (otherwise we'd have to silently fail - we can't tell the
                 # user, as that would reveal the existence of accounts with
                 # this email address)
                 for user_dict in user_list:
-                    logic.get_action(u'user_show')(
-                        context, {u'id': user_dict[u'id']})
-                    user_objs.append(context[u'user_obj'])
-                
+                    # type_ignore_reason: `user_list` returned the users,
+                    #                     so we know they exist here.
+                    user_objs.append(
+                        model.User.get(user_dict['id']))  # type: ignore
 
-        if not user_objs:
-            log.info("USER NOT FOUND IF STATEMENT")
-            log.info(u'User requested reset link for unknown user: {}'
-                        .format(id))
+        else:
+            # Search by user name
+            # (this is helpful as an option for a user who has multiple
+            # accounts with the same email address and they want to be
+            # specific)
+            user_obj = model.User.get(id)
+            if user_obj:
+                user_objs.append(user_obj)
+
 
         for user_obj in user_objs:
             log.info(u'Emailing reset link to user: {}'
-                        .format(user_obj.name))
+                     .format(user_obj.name))
             try:
+                # FIXME: How about passing user.id instead? Mailer already
+                # uses model and it allow to simplify code above
                 mailer.send_reset_link(user_obj)
             except mailer.MailerException as e:
-                h.flash_error(
-                    _(u'Error sending the email. Try again later '
-                        'or contact an administrator for help')
-                )
+                # SMTP is not configured correctly or the server is
+                # temporarily unavailable
+                h.flash_error(_(u'Error sending the email. Try again later '
+                                'or contact an administrator for help'))
                 log.exception(e)
-                return h.redirect_to(u'/')
+                return h.redirect_to(u'/password/reset')
+
+        # commit any final changes and remove session
+        model.repo.commit_and_remove()
+
         # always tell the user it succeeded, because otherwise we reveal
         # which accounts exist or not
         h.flash_success(
             _(u'A reset link has been emailed to you '
-                '(unless the account specified does not exist)'))
-        return h.redirect_to(u'/')
-    return render('user/request_reset.html')
+              '(unless the account specified does not exist)'))
+        return h.redirect_to(u'/password/reset')
+
+    def get(self):
+        self._prepare()
+        return base.render(u'user/request_reset.html', {})
+
+    
+reset_password_blueprint.add_url_rule(
+    u'/reset', view_func=RequestResetPasswordView.as_view(str(u'request_reset')))
